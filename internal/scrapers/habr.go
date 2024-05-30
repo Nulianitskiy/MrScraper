9751d820f2b97165a771154442831878d8ef613a
Nulianitskiy <nulianitskiy@gmail.com>
2024-05-30 20:53:06 +0300

    Смена user-agent и задержка запросов в habr

--- internal/scrapers/habr.go
+++ internal/scrapers/habr.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/gocolly/colly"
 	"log"
+	"net"
+	"net/http"
 	"strings"
+	"time"
 )
 
 type HabrScraper struct {
@@ -18,8 +21,27 @@ func (s HabrScraper) Scrap(theme string) ([]model.Article, error) {
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("habr.com"),
-		colly.UserAgent("user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"),
 	)
+
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+	}
+	uaIndex := 0
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", userAgents[uaIndex])
+		// Увеличиваем индекс для следующего запроса
+		uaIndex = (uaIndex + 1) % len(userAgents)
+	})
+
+	// Set a timeout for requests
+	c.WithTransport(&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout: 1 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout: 1 * time.Second,
+	})
 
 	c.OnHTML(".tm-articles-list__item", func(e *colly.HTMLElement) {
 		title := strings.TrimSpace(e.ChildText("a.tm-title__link"))
--- internal/scrapers/springer.go
+++ internal/scrapers/springer.go
@@ -18,8 +18,19 @@ func (s SpringerOpenScraper) Scrap(theme string) ([]model.Article, error) {
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.springeropen.com"),
-		colly.UserAgent("user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"),
 	)
+
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+	}
+	uaIndex := 0
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", userAgents[uaIndex])
+		// Увеличиваем индекс для следующего запроса
+		uaIndex = (uaIndex + 1) % len(userAgents)
+	})
 
 	c.OnHTML(".c-listing__content.u-mb-16", func(e *colly.HTMLElement) {
 		article := model.Article{}
